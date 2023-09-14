e100b35c86935fe1662591af4d89f803efbbc663
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-14 12:58:07 +0100

    Receive image after receiving length of image

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import io
+from PIL import Image
 
 # Config
 PORT = 4444
@@ -16,5 +18,9 @@ while running:
         with conn:
             print(f"New connection from {addr}")
             # Receive image size
-            n_bytes = conn.recv(4)
-            print(f"Image size: {int.from_bytes(n_bytes, 'big')}")
+            n_bytes = int.from_bytes(conn.recv(4), "big")
+            # Now receive whole image
+            img_bytes = conn.recv(n_bytes)
+            # Open the image
+            img = Image.open(io.BytesIO(img_bytes))
+            img.save("received.png")
